8ae2edb1f8a81d58ff55a993c1b17444987b89c4
renan martins <renanmartins1813@gmail.com>
2025-08-01 03:06:48 -0500

    more nvim :)

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -56,10 +56,10 @@ return {
 		incremental_selection = {
 			enable = true,
 			keymaps = {
-			  init_selection = "gnn", -- set to `false` to disable one of the mappings
-			  node_incremental = "grn",
+			  init_selection = "<Enter>", -- set to `false` to disable one of the mappings
+			  node_incremental = "<Enter>",
 			  scope_incremental = "grc",
-			  node_decremental = "grm",
+			  node_decremental = "<Backspace>",
 			},
 		},
 	},
