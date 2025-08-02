1c5a991795f0f97e806bcd01b464354da1ac4ce8
renan martins <renanmartins1813@gmail.com>
2025-08-02 03:53:11 -0500

    ne ne ne neovim

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -60,6 +60,19 @@ return { -- Autocompletion
 
 			-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
 			--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+
+			["<C-l>"] = {
+				function(cmp)
+					if cmp.snippet_active() then
+						return cmp.accept()
+					else
+						return cmp.select_and_accept()
+					end
+				end,
+				"snippet_forward",
+				"fallback",
+			},
+			["<C-h>"] = { "snippet_backward", "fallback" },
 		},
 
 		appearance = {
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,43 +1,43 @@
 return {
 	-- Highlight, edit, and navigate code
-	'nvim-treesitter/nvim-treesitter',
-	build = ':TSUpdate',
-	main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	main = "nvim-treesitter.configs", -- Sets main module to use for opts
 	-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 	opts = {
-		ensure_installed = { 
-			'astro',
-			'bash',
-			'c',
-			'c_sharp',
-			'cmake',
-			'cpp',
-			'css',
-			'diff',
-			'fish',
-			'gitignore',
-			'go',
-			'graphql',
-			'html',
-			'http',
-			'java',
-			'javascript',
-			'lua',
-			'luadoc',
-			'markdown',
-			'markdown_inline',
-			'php',
-			'query',
-			'regex',
-			'rust',
-			'scss',
-			'sql',
-			'styled',
-			'svelte',
-			'tsx',
-			'typescript',
-			'vim',
-			'vimdoc',
+		ensure_installed = {
+			"astro",
+			"bash",
+			"c",
+			"c_sharp",
+			"cmake",
+			"cpp",
+			"css",
+			"diff",
+			"fish",
+			"gitignore",
+			"go",
+			"graphql",
+			"html",
+			"http",
+			"java",
+			"javascript",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"php",
+			"query",
+			"regex",
+			"rust",
+			"scss",
+			"sql",
+			"styled",
+			"svelte",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
 		},
 
 		-- Autoinstall languages that are not installed
@@ -45,28 +45,28 @@ return {
 
 		highlight = {
 			enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = { 'ruby' },
+			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+			--  If you are experiencing weird indenting issues, add the language to
+			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
+			additional_vim_regex_highlighting = { "ruby" },
 		},
 
-		indent = { enable = true, disable = { 'ruby' } },
+		indent = { enable = true, disable = { "ruby" } },
 
 		incremental_selection = {
 			enable = true,
 			keymaps = {
-			  init_selection = "<Enter>", -- set to `false` to disable one of the mappings
-			  node_incremental = "<Enter>",
-			  scope_incremental = "grc",
-			  node_decremental = "<Backspace>",
+				init_selection = "<Enter>", -- set to `false` to disable one of the mappings
+				node_incremental = "<Enter>",
+				scope_incremental = "grc",
+				node_decremental = "<Backspace>",
 			},
 		},
 	},
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+	-- There are additional nvim-treesitter modules that you can use to interact
+	-- with nvim-treesitter. You should go explore a few and see what interests you:
+	--
+	--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+	--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+	--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
